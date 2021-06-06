65692f608c6bde88fd3ed3504859722ec3877f03
Cleiton Marcelino <cleitonmarcelino18@gmail.com>
2021-06-05 21:05:43 -0300

    Cadastro funcionando

--- js/script.js
+++ js/script.js
@@ -1,5 +1,53 @@
 $(document).ready(function(){
   $('.parallax').parallax();
   $('.sidenav').sidenav();
-  $('.modal').modal();
+});
+var app = angular.module('mesaApp', []);
+app.controller('mesaController', function ($scope, mesaService) {
+    $scope.mesa = {};    
+    listar();    
+    function listar() {
+      mesaService.listar().then(function(resposta) {
+            $scope.mesas = resposta.data;
+        });
+    }
+    
+    $scope.salvar = function(mesa) {
+      mesaService.salvar(mesa).then(listar);
+        $scope.mesa = {};
+    };
+    
+    $scope.editar = function(mesa) {
+        $scope.mesa = angular.copy(mesa);
+    };
+    
+    $scope.excluir = function(mesa) {
+        mesaService.excluir(mesa).then(listar);
+    };
+    
+    $scope.cancelar = function() {
+        $scope.mesa = {};
+    };
+});
+
+app.service('mesaService', function($http) {
+    
+    var api = 'http://localhost:3000/mesas';
+    
+    this.listar = function() {
+        return $http.get(api);
+    };
+
+    this.salvar = function(mesa) {
+        if(mesa.id) {
+           return $http.put(api + '/' + mesa.id, mesa);
+        } else {
+           return $http.post(api, mesa);
+        }
+    };
+    
+    this.excluir = function(mesa) {
+        return $http.delete(api + '/' + mesa.id);
+    };
+
 });
